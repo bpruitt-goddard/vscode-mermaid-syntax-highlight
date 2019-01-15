0a29c16b4894d64e8bb56322b8ecef578d7aa8f9
Brian Pruitt-Goddard <bpruitt-goddard@github>
2019-01-15 10:54:49 -0800

    feat: Create yaml custom type for regexes
    Update build process to use new type

---
+++ build/ConvertYaml.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var fs = require('fs');
+var jsYaml = require('js-yaml');
+var GRAMMAR_SCHEMA = require('./customYamlTypes').GRAMMAR_SCHEMA;
+
+const FILE_IN = "syntaxes/mermaid.tmLanguage.yaml",
+    FILE_OUT = "syntaxes/mermaid.tmLanguage.json";
+
+var res = jsYaml.load(fs.readFileSync(FILE_IN, "utf8"), { schema: GRAMMAR_SCHEMA });
+
+var vfd = fs.openSync(FILE_OUT, "w");
+fs.writeSync(vfd, JSON.stringify(res, null, 2), null, "utf8");
+fs.closeSync(vfd);
