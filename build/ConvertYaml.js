68569ab41a758e34b32dd673aa6adab14e6b6ca8
Brian Pruitt-Goddard <bpruitt-goddard@firstam.com>
2020-12-21 14:23:24 -0800

    feat: move all diagrams to separate files
    using yaml-import library to stitch files together

--- build/ConvertYaml.js
+++ build/ConvertYaml.js
@@ -1,14 +1,15 @@
 "use strict";
 
 var fs = require('fs');
-var jsYaml = require('js-yaml');
 var GRAMMAR_SCHEMA = require("./CustomYamlTypes").GRAMMAR_SCHEMA;
+var read = require('yaml-import').read;
+var path = require('path');
 
-const FILE_IN = "syntaxes/mermaid.tmLanguage.yaml",
-    FILE_OUT = "syntaxes/mermaid.tmLanguage.json";
+const FILE_IN = '../syntaxes/mermaid.tmLanguage.yaml',
+    FILE_OUT = '../syntaxes/mermaid.tmLanguage.json';
 
-var res = jsYaml.load(fs.readFileSync(FILE_IN, "utf8"), { schema: GRAMMAR_SCHEMA });
+const res = read(path.join(__dirname,  FILE_IN), null, GRAMMAR_SCHEMA) ;
 
-var vfd = fs.openSync(FILE_OUT, "w");
+var vfd = fs.openSync(path.join(__dirname, FILE_OUT), "w");
 fs.writeSync(vfd, JSON.stringify(res, null, 2), null, "utf8");
 fs.closeSync(vfd);
