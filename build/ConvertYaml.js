8b62f487cb7a89afcd152febfbf47f5d4787657f
Brian Pruitt-Goddard <bpruitt-goddard@github>
2022-06-10 12:46:22 -0700

    feat: create separate markdown mermaid file
    Convert both from yaml during prepackage

--- build/ConvertYaml.js
+++ build/ConvertYaml.js
@@ -5,13 +5,21 @@ var GRAMMAR_SCHEMA = require("./CustomYamlTypes").GRAMMAR_SCHEMA;
 var read = require('yaml-import').read;
 var path = require('path');
 
-const FILE_IN = '../syntaxes/mermaid.tmLanguage.yaml',
+const FILE_DIR = '../syntaxes/',
     DIR_OUT = '../out',
-    FILE_OUT = `${DIR_OUT}/mermaid.tmLanguage.json`;
-
-const res = read(path.join(__dirname,  FILE_IN), null, GRAMMAR_SCHEMA) ;
+    INPUT_DIR = path.join(__dirname, FILE_DIR),
+    OUTPUT_DIR = path.join(__dirname, DIR_OUT);
 
 fs.ensureDirSync(path.join(__dirname, DIR_OUT));
-fs.writeFileSync(path.join(__dirname, FILE_OUT), JSON.stringify(res, null, 2), {
-    encoding: "utf8"
-});
+
+fs.readdirSync(path.join(__dirname, FILE_DIR))
+    .filter(file => path.extname(file) === '.yaml')
+    .forEach(file => {
+        var inputName = path.join(INPUT_DIR, file);
+        var outputName = path.join(OUTPUT_DIR, path.parse(inputName).name + ".json");
+        var res = read(inputName, null, GRAMMAR_SCHEMA) ;
+        
+        fs.writeFileSync(outputName, JSON.stringify(res, null, 2), {
+            encoding: "utf8"
+        });
+    });
