938bbe14c1cd09de43b1f8cfc7518e6a155ec384
Kevin O'Shea <kevin@myuid.com>
2024-11-08 17:14:17 -0600

    update dependencies and use esm

--- build/ConvertYaml.mjs
+++ build/ConvertYaml.mjs
@@ -1,9 +1,11 @@
-'use strict';
+import fs from 'fs-extra';
+import { GRAMMAR_SCHEMA } from './CustomYamlTypes.mjs';
+import { read } from 'yaml-import';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-var fs = require('fs-extra');
-var GRAMMAR_SCHEMA = require('./CustomYamlTypes').GRAMMAR_SCHEMA;
-var read = require('yaml-import').read;
-var path = require('path');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const FILE_DIR = '../syntaxes/',
   DIR_OUT = '../out',
@@ -15,12 +17,12 @@ fs.ensureDirSync(path.join(__dirname, DIR_OUT));
 fs.readdirSync(path.join(__dirname, FILE_DIR))
   .filter((file) => path.extname(file) === '.yaml')
   .forEach((file) => {
-    var inputName = path.join(INPUT_DIR, file);
-    var outputName = path.join(
+    const inputName = path.join(INPUT_DIR, file);
+    const outputName = path.join(
       OUTPUT_DIR,
       path.parse(inputName).name + '.json',
     );
-    var res = read(inputName, null, GRAMMAR_SCHEMA);
+    const res = read(inputName, { schema: GRAMMAR_SCHEMA });
 
     fs.writeFileSync(outputName, JSON.stringify(res, null, 2), {
       encoding: 'utf8',
